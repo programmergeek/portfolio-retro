932254b7659fd0d60384e72ec71106b504e0061d
Thato Will Joseph <programergeek@gmail.com>
2024-11-12 21:15:00 +0200

    style: setup tailwind styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,34 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import theme, { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
+  darkMode: "selector",
   theme: {
     extend: {
+      fontSize: {
+        'header-1': '2.5rem',
+        'header-2': '2rem',
+        'header-3': '1.5rem',
+        'paragraph': '1rem',
+        'small': '0.875rem',
+        'copyright': '0.75rem'
+      },
+      colors: {
+        'off-black': '#221E1E',
+        'rouge-900': '#2E1310',
+        'rouge-800': '#561B1A',
+        'rouge-700': '#812123',
+        'rouge-600': '#AE242D',
+        'rouge-500': '#DE2437',
+        'rouge-400': '#EF6A64',
+        'rouge-300': '#FA9E95',
+        'rouge-200': '#FFCFC9',
+        'rouge-100': '#FFE7E4',
+      },
       fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        'concert-one': ['Concert One', 'sans-serif'],
+        'dm-mono': ['DM Mono', 'monospace']
       },
     },
   },
