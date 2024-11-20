6f0f047f7e220bf2e8508c95f341b3c4fe26fe91
Thato Will Joseph <programergeek@gmail.com>
2024-11-20 22:25:51 +0200

    feat[new:component]: navigation menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
-import { TRPCReactProvider } from "~/trpc/react";
-
 export const metadata: Metadata = {
   title: "Thato Will Joseph",
   description: "I love starberry milk. I'm lactose intolerant.",
@@ -17,8 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+          {children}
       </body>
-    </html>
+    </html >
   );
 }
